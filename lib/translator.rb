c31c036101c0844062c643c21bb12144d9c5f94f
yash patel <yash82099@yahoo.com>
2019-11-08 17:57:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,6 +7,9 @@ def load_library(file_path)
     result["get_meaning"][emoticons[1]] = meaning
     result["get_emoticon"][emoticons[0]] = emoticons[1]
 end
+result
+end 
+
 def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
   library = load_library(file_path)
   if library["get_emoticon"].include?(english_emoticon) 
@@ -21,3 +24,6 @@ def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
     library["get_meaning"][japanese_emoticon]
   else
     "Sorry, that emoticon was not found"
+  end 
+end 
+
