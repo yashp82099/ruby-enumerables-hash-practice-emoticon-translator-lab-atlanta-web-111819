1d1d6758a3dec205f6060c29afdf119de7228eb1
yash patel <yash82099@yahoo.com>
2019-11-08 17:56:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,6 +13,7 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
     library["get_emoticon"][english_emoticon]
   else
     "Sorry, that emoticon was not found"
+  end 
 end
 def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
   library = load_library(file_path)
