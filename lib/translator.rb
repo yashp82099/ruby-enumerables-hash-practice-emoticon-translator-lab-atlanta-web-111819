45eb70290c3fa9b739d5f73b8a5d8569366a21d2
yash patel <yash82099@yahoo.com>
2019-11-08 17:55:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,2 +1,11 @@
 require 'pry'
 require 'yaml'
+def load_library(file_path)
+  library = YAML.load_file(file_path)
+  result = {"get_meaning" => {}, "get_emoticon" => {}}
+  library.each do |meaning, emoticons|
+    result["get_meaning"][emoticons[1]] = meaning
+    result["get_emoticon"][emoticons[0]] = emoticons[1]
+  end
+  result
+end
